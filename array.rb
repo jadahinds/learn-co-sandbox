e219079daccb2c1f21fb4229163c114c2565380e
jadahinds <jhinds1229@pinescharter.net>
2018-07-17 20:13:24 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,25 @@
+celebcrushes = ["drake", "cole sprouse", "leo","chris brown", "beyonce"]
+
+puts celebcrushes[0]
+
+
+
+
+celebcrushes[0]= "Whitney Houston"
+puts celebcrushes
+
+
+
+celebcrushes << "rihanna"
+puts celebcrushes
+
+
+
+def firstPosition(arr)
+  return arr[0]
+end
+celebcrushes = ["drake", "cole sprouse", "leo","chris brown", "beyonce"]
+
+name = firstPosition(celebcrushes)
+
+puts name
---
+++ array2.rb
@@ -0,0 +1,10 @@
+names = ["jada", "priscilla", "kristina"]
+
+puts names
+
+puts "what is you name?"
+name= gets.chomp
+
+names << "#{name}"
+puts names
+
